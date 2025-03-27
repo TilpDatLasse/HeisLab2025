c6685ffa94f4b2e7e277dab68cebca5668c7ff04
martbn <martinbrekkenilsen@gmail.com>
2025-03-27 08:38:08 +0100

    fingjort peers.go

--- network/peers/peers.go
+++ network/peers/peers.go
@@ -1,12 +1,8 @@
 package peers
 
 import (
-	"fmt"
-	"net"
 	"sort"
 	"time"
-
-	"github.com/TilpDatLasse/HeisLab2025/network/conn"
 )
 
 type PeerUpdate struct {
@@ -16,93 +12,26 @@ type PeerUpdate struct {
 	Id    string //mulig dette ikke går
 }
 
-const interval = 15 * time.Millisecond
-const timeout = 5000 * time.Millisecond
+const (
+	interval = 15 * time.Millisecond
+	timeout  = 5000 * time.Millisecond
+)
 
-var PeerToUpdate = make(chan PeerUpdate)
-var PeerFromUpdate = make(chan PeerUpdate)
+var (
+	PeerToUpdate   = make(chan PeerUpdate)
+	PeerFromUpdate = make(chan PeerUpdate)
+)
 
-func Transmitter(port int, id string, transmitEnable <-chan bool) {
-
-	conn := conn.DialBroadcastUDP(port)
-	addr, _ := net.ResolveUDPAddr("udp4", fmt.Sprintf("255.255.255.255:%d", port))
-
-	enable := true
-	for {
-		select {
-		case enable = <-transmitEnable:
-		case <-time.After(interval):
-		}
-		if enable {
-			conn.WriteTo([]byte(id), addr)
-		}
-	}
-}
-
-func Receiver(port int, peerUpdateCh chan<- PeerUpdate) {
-
-	var buf [1024]byte
-	var p PeerUpdate
-	lastSeen := make(map[string]time.Time)
-
-	conn := conn.DialBroadcastUDP(port)
-
-	for {
-		updated := false
-
-		conn.SetReadDeadline(time.Now().Add(interval))
-		n, _, _ := conn.ReadFrom(buf[0:])
-
-		id := string(buf[:n])
-
-		// Adding new connection
-		p.New = ""
-		if id != "" {
-			if _, idExists := lastSeen[id]; !idExists {
-				p.New = id
-				updated = true
-			}
-
-			lastSeen[id] = time.Now()
-		}
-
-		// Removing dead connection
-		p.Lost = make([]string, 0)
-		for k, v := range lastSeen {
-			if time.Now().Sub(v) > timeout {
-				updated = true
-				p.Lost = append(p.Lost, k)
-				delete(lastSeen, k)
-			}
-		}
-
-		// Sending update
-		if updated {
-			p.Peers = make([]string, 0, len(lastSeen))
-
-			for k, _ := range lastSeen {
-				p.Peers = append(p.Peers, k)
-			}
-
-			sort.Strings(p.Peers)
-			sort.Strings(p.Lost)
-			peerUpdateCh <- p
-		}
-	}
-}
-
-func UpdatePeers() { //peerToUpdate chan PeerUpdate, peerFromUpdate chan PeerUpdate
+// Update peers that are online
+func UpdatePeers() {
 	var p PeerUpdate
 	lastSeen := make(map[string]time.Time)
 
 	for {
 		updated := false
-
 		p = <-PeerToUpdate
-
-		// fmt.Println("UPDATE", p)
-
 		id := p.Id
+
 		// Adding new connection
 		p.New = ""
 		if id != "" {
@@ -113,7 +42,6 @@ func UpdatePeers() { //peerToUpdate chan PeerUpdate, peerFromUpdate chan PeerUpd
 
 			lastSeen[id] = time.Now()
 		}
-
 		// Removing dead connection
 		p.Lost = make([]string, 0)
 		for k, v := range lastSeen {
@@ -123,15 +51,12 @@ func UpdatePeers() { //peerToUpdate chan PeerUpdate, peerFromUpdate chan PeerUpd
 				delete(lastSeen, k)
 			}
 		}
-
 		// Sending update
 		if updated {
 			p.Peers = make([]string, 0, len(lastSeen))
-
 			for k, _ := range lastSeen {
 				p.Peers = append(p.Peers, k)
 			}
-
 			sort.Strings(p.Peers)
 			sort.Strings(p.Lost)
 			PeerFromUpdate <- p
