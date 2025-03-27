74de1165bc2bab546edea1ce29683b659df34b41
Lasse <lassefprael@gmail.com>
2025-03-27 09:45:02 +0100

    ryddet i sync

---
+++ worldview/conversion.go
@@ -0,0 +1,68 @@
+package worldview
+
+import (
+	elev "github.com/TilpDatLasse/HeisLab2025/elev_algo/elevator_io"
+)
+
+// Converting an elev.Elevator-variabel to an InformationElev-variabel
+func Converter(e elev.Elevator) InformationElev {
+	rawInput := e
+	hallRequests := make([][2]elev.ConfirmationState, len(rawInput.Requests))
+	cabRequests := make([]elev.ConfirmationState, len(rawInput.Requests))
+
+	for i := 0; i < len(rawInput.Requests); i++ {
+		hallRequests[i] = [2]elev.ConfirmationState{rawInput.Requests[i][0], rawInput.Requests[i][1]}
+		cabRequests[i] = rawInput.Requests[i][2]
+	}
+
+	input := InformationElev{
+		HallRequests: hallRequests,
+		State: HRAElevState{
+			Behavior:    stateToString(rawInput.State),
+			Floor:       rawInput.Floor,
+			Direction:   dirnToString(rawInput.Dirn),
+			CabRequests: cabToBool(cabRequests),
+		},
+	}
+	return input
+}
+
+// Converting elev.state to HRAElevState.Behaviour (string)
+func stateToString(s elev.State) string {
+	switch s {
+	case elev.IDLE:
+		return "idle"
+	case elev.MOVE:
+		return "moving"
+	case elev.DOOROPEN:
+		return "doorOpen"
+	case elev.STOP:
+		return "doorOpen"
+	default:
+		return "idle"
+	}
+}
+
+// Converting elev.MotorDirection to HRAElevState.Direction
+func dirnToString(s elev.MotorDirection) string {
+	switch s {
+	case elev.MD_Up:
+		return "up"
+	case elev.MD_Down:
+		return "down"
+	case elev.MD_Stop:
+		return "stop"
+	default:
+		return "stop"
+	}
+}
+
+// Converting cabrequests from ConfirmationState (cyclic-counter) to bool
+// Notice that cab-requests do not need to be confirmed, so both confirmationstates 1 and 2 will yield true
+func cabToBool(list []elev.ConfirmationState) []bool {
+	boolList := make([]bool, len(list))
+	for i, v := range list {
+		boolList[i] = v != 0
+	}
+	return boolList
+}
--- worldview/worldview.go
+++ worldview/worldview.go
@@ -174,68 +174,6 @@ func SetElevatorStatus(ch_WVTx chan WorldView) {
 	}
 }
 
-// konverterer en elev.elevator-variabel til en InformationElev-variabel
-func Converter(e elev.Elevator) InformationElev {
-	rawInput := e
-	hallRequests := make([][2]elev.ConfirmationState, len(rawInput.Requests))
-	cabRequests := make([]elev.ConfirmationState, len(rawInput.Requests))
-
-	for i := 0; i < len(rawInput.Requests); i++ {
-		hallRequests[i] = [2]elev.ConfirmationState{rawInput.Requests[i][0], rawInput.Requests[i][1]}
-		cabRequests[i] = rawInput.Requests[i][2]
-	}
-
-	input := InformationElev{
-		HallRequests: hallRequests,
-		State: HRAElevState{
-			Behavior:    stateToString(rawInput.State),
-			Floor:       rawInput.Floor,
-			Direction:   dirnToString(rawInput.Dirn),
-			CabRequests: cabToBool(cabRequests),
-		},
-	}
-	return input
-}
-
-// Converting elev.state to HRAElevState.Behaviour (string)
-func stateToString(s elev.State) string {
-	switch s {
-	case elev.IDLE:
-		return "idle"
-	case elev.MOVE:
-		return "moving"
-	case elev.DOOROPEN:
-		return "doorOpen"
-	case elev.STOP:
-		return "doorOpen"
-	default:
-		return "idle"
-	}
-}
-
-// Converting elev.MotorDirection to HRAElevState.Direction
-func dirnToString(s elev.MotorDirection) string {
-	switch s {
-	case elev.MD_Up:
-		return "up"
-	case elev.MD_Down:
-		return "down"
-	case elev.MD_Stop:
-		return "stop"
-	default:
-		return "stop"
-	}
-}
-
-// Converting cabrequests from ConfirmationState (cyclic-counter) to bool
-// Notice that cab-requests do not need to be confirmed, so both confirmationstates 1 and 2 will yield true
-func cabToBool(list []elev.ConfirmationState) []bool {
-	boolList := make([]bool, len(list))
-	for i, v := range list {
-		boolList[i] = v != 0
-	}
-	return boolList
-}
 
 func wasTimedOut() bool {
 	var timeOut float64 = 1.0
